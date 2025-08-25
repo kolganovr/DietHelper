4686f9ea120494e0a530980f71f1992f97b8ca0f
Roman Kolganov <kolganov2003@gmail.com>
2025-08-25 15:09:40 +0300

    Simplify Vercel deployment with a single build step

--- DietHelper/settings.py
+++ DietHelper/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- build_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Устанавливаем зависимости
-pip install -r requirements.txt
-
-# Собираем статические файлы
-python manage.py collectstatic --noinput --clear
