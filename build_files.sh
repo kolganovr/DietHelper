179720ed84700cdc87e80006527d0540818ae528
Roman Kolganov <kolganov2003@gmail.com>
2025-08-25 15:05:17 +0300

    Fix Vercel build configuration

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Build the project
-echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "Build finished."
+# Устанавливаем зависимости
+pip install -r requirements.txt
+
+# Собираем статические файлы
+python manage.py collectstatic --noinput --clear
