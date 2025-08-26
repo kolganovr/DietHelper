69f0f846ecd3716809e421656d41086369637b87
Roman Kolganov <kolganov2003@gmail.com>
2025-08-26 17:12:07 +0300

    Add error handling to get_verdict function

--- diet/gemini.py
+++ diet/gemini.py
@@ -12,15 +12,19 @@ class Answer(BaseModel):
 def get_verdict(product, diet_description, api_key):
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
-        config=types.GenerateContentConfig(
-            response_mime_type="application/json",
-            response_schema=Answer,
-            system_instruction="Ты профессиональный диетолог, твоя задача по описанию блюда или его составу а также диете пользователя вынести вердикт можно ли есть его или нельзя. Доступные варианты ответа \"Да\", \"Нет\", \"С осторожностью\". Также ты должен предоставить краткое объяснение своего решения."
-        ),
-        contents=f"Блюдо: '{product}', Диета: '{diet_description}'"
-    )
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash-lite",
+            config=types.GenerateContentConfig(
+                response_mime_type="application/json",
+                response_schema=Answer,
+                system_instruction="Ты профессиональный диетолог, твоя задача по описанию блюда или его составу а также диете пользователя вынести вердикт можно ли есть его или нельзя. Доступные варианты ответа \"Да\", \"Нет\", \"С осторожностью\". Также ты должен предоставить краткое объяснение своего решения."
+            ),
+            contents=f"Блюдо: '{product}', Диета: '{diet_description}'"
+        )
+    except Exception as e:
+        print(f"Ошибка при получении ответа от модели: {e}")
+        return "Ошибка", "Не удалось получить ответ от модели. Попробуйте позже."
 
     # parse json
     response_data = json.loads(response.text)
